1ad2cedeb63d6c16b7b3f23dad5d59186ad04f29
IT Smooth <itsmooth.tw@gmail.com>
2019-12-13 13:10:44 +0800

    完成Homework-2：資料來自 db。 1.加入老師給的DB； 2.使用 來自資料庫的codefirst 建立 model  ; 3. Memoy controlles 的資料來源，從餵假資料改成從 db 讀取資料

--- mvcEX1/Controllers/MoneyController.cs
+++ mvcEX1/Controllers/MoneyController.cs
@@ -1,4 +1,5 @@
-﻿using mvcEX1.Models.ViewModels;
+﻿using mvcEX1.Models;
+using mvcEX1.Models.ViewModels;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -12,20 +13,39 @@ namespace mvcEX1.Controllers
         // GET: Money
         public ActionResult Index()
         {
-            var moneys = new List<Money>();
-            var count = 300;
-            DateTime moneyDate = DateTime.Parse("2010-01-01");
-            var r = new Random();
 
-            for (int id = 0; id < count; id++)
+            var db = new SkillTreeHomework();
+            var accountBook = db.AccountBook;
+            var moneys = new List<Money>();
+
+            //var count = 300;
+            //DateTime moneyDate = DateTime.Parse("2010-01-01");
+            //var r = new Random();
+            //for (int id = 0; id < count; id++)
+            //{
+            //    moneys.Add(new Money(id)
+            //    {
+            //        MoneyType = (r.Next(0, 2) == 0 ? MoneyTypeEnum.Income : MoneyTypeEnum.Expend),
+            //        Amount = r.Next(1, 100000),
+            //        Date = (moneyDate = moneyDate.AddDays(r.Next(1, 30))),
+            //    });
+            //}
+
+            int id = 0;
+            foreach(var item in accountBook)
             {
+                // TODO ：當初訂 Viewmodel 裡的 ID 為 int，與實際資料庫內類型不同，加上 Viewodel現在不能改，暫時沒把 db 裡的 ID 放進來
                 moneys.Add(new Money(id)
                 {
-                    MoneyType = (r.Next(0, 2) == 0 ? MoneyTypeEnum.Income : MoneyTypeEnum.Expend),
-                    Amount = r.Next(1, 100000),
-                    Date = (moneyDate = moneyDate.AddDays(r.Next(1, 30))),
+                    // TODO : 待確認： item.Categoryyy 0是收入??、 1是支出??
+                    MoneyType = (item.Categoryyy == 0 ? MoneyTypeEnum.Income : MoneyTypeEnum.Expend),
+                    Amount = (decimal)item.Amounttt,
+                    Date = item.Dateee,
+                    Description = item.Remarkkk
                 });
+                id++;
             }
+
             return View(moneys);
         }
     }
