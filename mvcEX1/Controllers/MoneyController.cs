f384e369a9f4d98f0adef2aadf3a57f6fe01b576
IT Smooth <itsmooth.tw@gmail.com>
2019-12-13 11:19:11 +0800

    完成第一次回家作業：1.建立 Viewmodels、Money controller、顯示頁面 。2.餵300筆假資料以測試之

--- mvcEX1/Controllers/MoneyController.cs
+++ mvcEX1/Controllers/MoneyController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using mvcEX1.Models.ViewModels;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -11,7 +12,21 @@ namespace mvcEX1.Controllers
         // GET: Money
         public ActionResult Index()
         {
-            return View();
+            var moneys = new List<Money>();
+            var count = 300;
+            DateTime moneyDate = DateTime.Parse("2010-01-01");
+            var r = new Random();
+
+            for (int id = 0; id < count; id++)
+            {
+                moneys.Add(new Money(id)
+                {
+                    MoneyType = (r.Next(0, 2) == 0 ? MoneyTypeEnum.Income : MoneyTypeEnum.Expend),
+                    Amount = r.Next(1, 100000),
+                    Date = (moneyDate = moneyDate.AddDays(r.Next(1, 30))),
+                });
+            }
+            return View(moneys);
         }
     }
 }
---
+++ mvcEX1/Models/ViewModels/Money.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace mvcEX1.Models.ViewModels
+{
+    public class Money
+    {
+        private int id;
+        public int ID { get { return this.id; } }
+        public MoneyTypeEnum MoneyType { get; set; }
+        public decimal Amount { get; set; }
+        public DateTime Date { get; set; }
+        public string Description { get; set; }
+        public Money (int id)
+        {
+            this.id = id;
+        }
+    }
+
+    public enum MoneyTypeEnum
+    {
+        Income = 0,
+        Expend = 1
+    }
+}
