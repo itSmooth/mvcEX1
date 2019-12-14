56c9d07f3196dc0512c097078f195dfa04bc7985
IT Smooth <itsmooth.tw@gmail.com>
2019-12-14 20:58:53 +0800

    MoneyViewModel clss 命名加上 ViewModel 後綴、檔名也重新命名；MoneyViewModel  id 類型改成符合 db 裡的類型

--- mvcEX1/Controllers/MoneyController.cs
+++ mvcEX1/Controllers/MoneyController.cs
@@ -16,34 +16,17 @@ namespace mvcEX1.Controllers
 
             var db = new SkillTreeHomework();
             var accountBook = db.AccountBook;
-            var moneys = new List<MoneyViewmodel>();
+            var moneys = new List<MoneyViewModel>();
 
-            //var count = 300;
-            //DateTime moneyDate = DateTime.Parse("2010-01-01");
-            //var r = new Random();
-            //for (int id = 0; id < count; id++)
-            //{
-            //    moneys.Add(new Money(id)
-            //    {
-            //        MoneyType = (r.Next(0, 2) == 0 ? MoneyTypeEnum.Income : MoneyTypeEnum.Expend),
-            //        Amount = r.Next(1, 100000),
-            //        Date = (moneyDate = moneyDate.AddDays(r.Next(1, 30))),
-            //    });
-            //}
-
-            int id = 0;
             foreach(var item in accountBook)
             {
-                // TODO ：當初訂 Viewmodel 裡的 ID 為 int，與實際資料庫內類型不同，加上 Viewodel現在不能改，暫時沒把 db 裡的 ID 放進來
-                moneys.Add(new MoneyViewmodel(id)
+                moneys.Add(new MoneyViewModel(item.Id)
                 {
-                    // TODO : 待確認： item.Categoryyy 0是收入??、 1是支出??
                     MoneyType = (item.Categoryyy == 0 ? MoneyTypeEnum.Income : MoneyTypeEnum.Expend),
                     Amount = (decimal)item.Amounttt,
                     Date = item.Dateee,
                     Description = item.Remarkkk
                 });
-                id++;
             }
 
             return View(moneys);
--- mvcEX1/Models/ViewModels/MoneyViewModel.cs
+++ mvcEX1/Models/ViewModels/MoneyViewModel.cs
@@ -5,15 +5,15 @@ using System.Web;
 
 namespace mvcEX1.Models.ViewModels
 {
-    public class MoneyViewmodel
+    public class MoneyViewModel
     {
-        private int id;
-        public int ID { get { return this.id; } }
+        private Guid id;
+        public Guid ID { get { return this.id; } }
         public MoneyTypeEnum MoneyType { get; set; }
         public decimal Amount { get; set; }
         public DateTime Date { get; set; }
         public string Description { get; set; }
-        public MoneyViewmodel (int id)
+        public MoneyViewModel (Guid id)
         {
             this.id = id;
         }
