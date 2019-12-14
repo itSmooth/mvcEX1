f773c2e2b7884c99556aa1c814eb5be117a268b5
IT Smooth <itsmooth.tw@gmail.com>
2019-12-14 09:43:44 +0800

    class 更改命名為 MoneyViewmodel

--- mvcEX1/Controllers/MoneyController.cs
+++ mvcEX1/Controllers/MoneyController.cs
@@ -16,7 +16,7 @@ namespace mvcEX1.Controllers
 
             var db = new SkillTreeHomework();
             var accountBook = db.AccountBook;
-            var moneys = new List<Money>();
+            var moneys = new List<MoneyViewmodel>();
 
             //var count = 300;
             //DateTime moneyDate = DateTime.Parse("2010-01-01");
@@ -35,7 +35,7 @@ namespace mvcEX1.Controllers
             foreach(var item in accountBook)
             {
                 // TODO ：當初訂 Viewmodel 裡的 ID 為 int，與實際資料庫內類型不同，加上 Viewodel現在不能改，暫時沒把 db 裡的 ID 放進來
-                moneys.Add(new Money(id)
+                moneys.Add(new MoneyViewmodel(id)
                 {
                     // TODO : 待確認： item.Categoryyy 0是收入??、 1是支出??
                     MoneyType = (item.Categoryyy == 0 ? MoneyTypeEnum.Income : MoneyTypeEnum.Expend),
--- mvcEX1/Models/ViewModels/Money.cs
+++ mvcEX1/Models/ViewModels/Money.cs
@@ -5,7 +5,7 @@ using System.Web;
 
 namespace mvcEX1.Models.ViewModels
 {
-    public class Money
+    public class MoneyViewmodel
     {
         private int id;
         public int ID { get { return this.id; } }
@@ -13,7 +13,7 @@ namespace mvcEX1.Models.ViewModels
         public decimal Amount { get; set; }
         public DateTime Date { get; set; }
         public string Description { get; set; }
-        public Money (int id)
+        public MoneyViewmodel (int id)
         {
             this.id = id;
         }
